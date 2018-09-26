09a3f7d5c32f15027d7975014f1f599e1b2325a7
djungelorm <djungelorm@users.noreply.github.com>
2018-09-26 23:12:29 +0100

    Fix crash when player dies, as item stack becomes invalid

--- control.lua
+++ control.lua
@@ -4,14 +4,19 @@ script.on_nth_tick(
       for index, player in pairs(game.connected_players) do
          if player.character then
             inventory = player.get_inventory(defines.inventory.player_armor)
-            for i=1, #inventory do
-               grid = inventory[i].grid
-               if grid ~= nil then
-                  for x=1, grid.width do
-                     for y=1, grid.height do
-                        item = grid.get({x, y})
-                        if item ~= nil and item.type == 'roboport-equipment' then
-                           item.energy = item.max_energy
+            if inventory ~= nil then
+               for i=1, #inventory do
+                  itemstack = inventory[i]
+                  if itemstack ~= nil and itemstack.valid and itemstack.valid_for_read then
+                     grid = itemstack.grid
+                     if grid ~= nil then
+                        for x=1, grid.width do
+                           for y=1, grid.height do
+                              item = grid.get({x, y})
+                              if item ~= nil and item.type == 'roboport-equipment' then
+                                 item.energy = item.max_energy
+                              end
+                           end
                         end
                      end
                   end
